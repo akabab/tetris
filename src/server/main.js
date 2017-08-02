54b1d79cca1f4cb6363c7d8f0b855d800e1a8197
Yoann Cribier <yoann_cribier@hotmail.com>
2017-08-02 17:40:51 +0200

    cleaned boilerplate

--- src/server/main.js
+++ src/server/main.js
@@ -1,3 +1,5 @@
-import params  from '../../params'
-import * as server from './index'
-server.create(params.server).then( () => console.log('not yet ready to play tetris with U ...') )
+const params = require('../../params')
+const server = require('./index')
+
+server.create(params.server)
+  .then( () => console.log('not yet ready to play tetris with U ...') )
